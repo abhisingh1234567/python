31b9c2db3eea7fca35437c70345e287b6aa28f05
abhishek <abhishek.singh3_cs21@gla.ac.in>
2022-05-30 01:36:37 +0530

    funct4

--- function/function.CAlculator.py
+++ function/function.CAlculator.py
@@ -8,6 +8,7 @@ def multiply(a,b):
     return a*b
 def divide(a,b):
     return a/b
+
 print("selection of the operators\n","1\n","2\n","3\n","4\n")
 selection=int(input("select the operator from 1,2,3,4\n"))
 a=float(input("enter the first value\n"))
