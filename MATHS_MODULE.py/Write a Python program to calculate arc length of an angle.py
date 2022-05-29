6bc15da34f3e1fe2340e9677b098077f6924d821
abhishek <abhishek.singh3_cs21@gla.ac.in>
2022-05-30 01:10:21 +0530

    module

--- MATHS_MODULE.py/Write a Python program to calculate arc length of an angle.py
+++ MATHS_MODULE.py/Write a Python program to calculate arc length of an angle.py
@@ -4,6 +4,8 @@ angle = float(input('angle measure: '))
 if angle < 360:
     len_arc=((pi*diameter)*(angle/360))
     print(len_arc)
+
+    
 else:
     print("angle is out of the  range")
         
