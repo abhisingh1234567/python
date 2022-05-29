6ad008ac6acd1d09fddfd40a3ac07543c8497b62
abhishek <abhishek.singh3_cs21@gla.ac.in>
2022-05-30 01:47:22 +0530

    module_randome10

--- RANDOM_MODULE.py/Write a Python program to select a random element from a list, set, dictionary (value) and a file from a directory.py
+++ RANDOM_MODULE.py/Write a Python program to select a random element from a list, set, dictionary (value) and a file from a directory.py
@@ -10,6 +10,7 @@ elements = set([1, 2, 3, 4, 5])
 print(random.choice(tuple(elements)))
 print(random.choice(tuple(elements)))
 print(random.choice(tuple(elements)))
+
 print("\nSelect a random value from a dictionary:")
 d = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}
 key = random.choice(list(d))
