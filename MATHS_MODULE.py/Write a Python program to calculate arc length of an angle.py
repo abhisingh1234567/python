b491389114e38902f70170ffec6165b139ae2d0c
abhishek <abhishek.singh3_cs21@gla.ac.in>
2022-05-30 01:11:25 +0530

    numpy done

--- MATHS_MODULE.py/Write a Python program to calculate arc length of an angle.py
+++ MATHS_MODULE.py/Write a Python program to calculate arc length of an angle.py
@@ -5,7 +5,7 @@ if angle < 360:
     len_arc=((pi*diameter)*(angle/360))
     print(len_arc)
 
-    
+
 else:
     print("angle is out of the  range")
         
--- NUMPY EXERCISE/Write a NumPy program to capitalize the first letter, lowercase,  #uppercase, swapcase, title-case of all the elements of a given array.py
+++ NUMPY EXERCISE/Write a NumPy program to capitalize the first letter, lowercase,  #uppercase, swapcase, title-case of all the elements of a given array.py
@@ -2,6 +2,7 @@ import numpy as np
 f=np.array(['whEre','Are','you','comIng','from'])
 print("before performing the oprations")
 print(f)
+
 print("after performing the operations")
 print(np.char.upper(f))
 print(np.char.lower(f))
