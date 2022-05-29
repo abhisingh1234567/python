18aaf7aad97412d404e124fad953735eb276bc24
abhishek <abhishek.singh3_cs21@gla.ac.in>
2022-05-30 01:40:36 +0530

    funct12

--- function/function_prime.py
+++ function/function_prime.py
@@ -7,5 +7,6 @@ for number in range (lower_value, upper_value + 1):
         for i in range (2, number):  
             if (number % i) == 0:  
                 break  
-        else:  
+        else:
+
             print (number)  
